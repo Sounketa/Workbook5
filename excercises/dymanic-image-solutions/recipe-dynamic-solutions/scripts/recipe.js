4195c7e67383df7fa29f27bb3f157fcf9b60632f
Sounketa <julien.ilboudo97@gmail.com>
2024-05-16 10:51:11 -0400

    more dynamic solutions

---
+++ excercises/dymanic-image-solutions/recipe-dynamic-solutions/scripts/recipe.js
@@ -0,0 +1,17 @@
+"use strict";
+
+window.onload = function () {
+  // HTML element variables
+  const imageFileDropdown = document.querySelector("#imageFileDropdown");
+  const addImageButton = document.querySelector("#addImageButton");
+  
+  //functions
+  function imageDropdown() {
+    
+  }
+
+  //event handling
+
+  //initial loading
+
+};
